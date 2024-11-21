139614525ede96fb0024b13159bb20c72aa68737
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2024-11-21 11:37:03 +0530

    Update 8bitMultiplication.asm

--- 8bitMultiplication.asm
+++ 8bitMultiplication.asm
@@ -4,7 +4,7 @@
 .data
     num1 db 20h         ; Define `num1` as a byte with the value `20h` (32 in decimal).
     num2 db 04h         ; Define `num2` as a byte with the value `04h` (4 in decimal).
-    res db ?            ; Define `res` as a byte to store the result of the multiplication.
+    res dw ?            ; Define `res` as a byte to store the result of the multiplication.
 
 .code
 main:
@@ -17,7 +17,7 @@ main:
     mul bl              ; Multiply AL by BL. The result is stored in AX.
                         ; Since both operands are 8-bit, the result will fit in AL (lower part of AX).
 
-    mov res, al         ; Store the result from AL into the `res` variable.
+    mov res, ax         ; Store the result from AL into the `res` variable.
 
     mov ah, 4ch         ; Set AH to 4Ch, the DOS function to terminate the program.
     int 21h             ; Call DOS interrupt 21h to exit the program.
